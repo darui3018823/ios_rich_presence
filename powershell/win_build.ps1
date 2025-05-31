e25dad90da2936595a318177a7acc1e083742af2
darui3018823 <n.sumikko.0711@gmail.com>
2025-05-31 12:07:53 +0900

    ps1の更新

--- powershell/win_build.ps1
+++
@@ -1,7 +0,0 @@
-mkdir ./dist/win/
-Remove-Item ./dist/win/ios_shortcut_rpc_serv.exe -Force
-$env:GOOS = "windows"
-$env:GOARCH = "amd64"
-go build -o ./dist/win/ios_shortcut_rpc_serv.exe main.go
-Remove-Item Env:GOOS
-Remove-Item Env:GOARCH
