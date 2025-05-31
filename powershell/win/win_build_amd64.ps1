edcf0651b0998670e7706389a771cf8b0dc2f9d6
darui3018823 <n.sumikko.0711@gmail.com>
2025-05-31 14:57:19 +0900

    Update: ps1

--- powershell/win/win_build_amd64.ps1
+++ powershell/win/win_build_amd64.ps1
@@ -1,6 +1,13 @@
+# Recomended PowerShell Core Version: 7.5.0 or later
+
+Write-Host "Building iOS Shortcut RPC Server for Windows AMD64..."
+Write-Host "Removing old executables..."
 Remove-Item ./dist/win/ios_shortcut_rpc_serv_amd64win.exe -Force
+
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
 go build -o ./dist/win/ios_shortcut_rpc_serv_amd64win.exe main.go
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
+Write-Host "Building iOS Shortcut RPC Server for Windows AMD64 complete."
+Write-Host "Server run command: ./dist/win/ios_shortcut_rpc_serv_amd64win.exe"
--- powershell/win/win_build_arm.ps1
+++ powershell/win/win_build_arm.ps1
@@ -1,6 +1,13 @@
+# Recomended PowerShell Core Version: 7.5.0 or later
+
+Write-Host "Building iOS Shortcut RPC Server for Windows ARM..."
+Write-Host "Removing old executables..."
 Remove-Item ./dist/win/ios_shortcut_rpc_serv_armwin.exe -Force
+
 $env:GOOS = "windows"
 $env:GOARCH = "arm"
 go build -o ./dist/win/ios_shortcut_rpc_serv_armwin.exe main.go
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
+Write-Host "Building iOS Shortcut RPC Server for Windows ARM complete."
+Write-Host "Server run command: ./dist/win/ios_shortcut_rpc_serv_armwin.exe"
