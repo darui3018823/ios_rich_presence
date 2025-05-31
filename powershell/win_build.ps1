968aeea8dc29bd59cdaddb7a3d68315ae503a8a3
darui3018823 <n.sumikko.0711@gmail.com>
2025-05-31 12:04:36 +0900

    update: ps1

--- powershell/win_build.ps1
+++ powershell/win_build.ps1
@@ -2,6 +2,6 @@ mkdir ./dist/win/
 Remove-Item ./dist/win/ios_shortcut_rpc_serv.exe -Force
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
-go build -o ./dist/win/ios_shortcut_rpc_serv.exe main.go log.go
+go build -o ./dist/win/ios_shortcut_rpc_serv.exe main.go
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
